30f0b40e4a9132b9e9b284a4ee844507294ec349
belyne <nzabonamarie@gmail.com>
2024-01-21 13:29:40 +0000

    MySQL_Advanced

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,14 +1,5 @@
--- 2. Best band ever!
-
--- Import the table dump
--- You can replace 'metal_bands.sql.zip' with the correct file path or URL
--- Ensure the file is accessible to the MySQL server
--- The command may vary based on the source of the dump (local file, URL, etc.)
--- Example command for a local file:
--- mysql -uroot -p holberton < metal_bands.sql
-
--- Query to rank country origins of bands by the number of (non-unique) fans
-SELECT origin, COUNT(*) as nb_fans
+-- Rank country origins of bands by the number of non-unique fans
+SELECT origin, COUNT(DISTINCT fan_id) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
